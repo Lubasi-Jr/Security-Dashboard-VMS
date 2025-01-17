3fe675c11bec5c3319a7f712caa7e3e027aee96c
Lubasi Milupi <lubasimilupi@gmail.com>
2025-01-17 14:47:31 +0200

    Modal added for CheckIn and Checking Out

--- src/components/Cards/VisitDetails.jsx
+++ src/components/Cards/VisitDetails.jsx
@@ -2,9 +2,26 @@ import React, { useState } from "react";
 import { useParams, Link } from "react-router-dom";
 import BackButton from "../Buttons/BackButton";
 import { DoorOpen } from "lucide-react";
+import { DoorClosed } from "lucide-react";
+import CheckInModal from "../Buttons/CheckInModal";
 
 const VisitDetails = () => {
   const { visitId } = useParams();
+  const [hideModal, setHideModal] = useState(true);
+  const [purpose, setPurpose] = useState("");
+
+  const checkVisitorInOrOut = () => {
+    if (purpose == "Check-In") {
+      //Execute Check-in route
+      console.log("Checking In visitor...");
+    } else {
+      //Execute Check-out route
+      console.log("Checking Out visitor...");
+    }
+
+    setHideModal(!hideModal);
+  };
+
   return (
     <div className="flex items-center justify-center text-3xl h-full bg-[#F5F5F5] md:pl-24 md:pt-8 px-20 pt-8 pb-28 md:pb-2">
       <div className="rounded-md w-full h-full bg-white px-4 py-4 flex flex-col gap-5">
@@ -72,11 +89,77 @@ const VisitDetails = () => {
         </div>
         <button
           type="button"
-          className="text-cecRed w-[250px] hover:text-white border border-cecRed bg-white hover:bg-cecRed font-medium rounded-lg text-sm md:text-md lg:text-lg px-5 py-2.5 text-center me-2 mb-2 flex gap-1 justify-center items-center"
+          purpose="Check-In"
+          onClick={() => {
+            setPurpose("Check-in");
+            setHideModal(!hideModal);
+          }}
+          className="text-cecOrange w-[250px] hover:text-white border border-cecOrange bg-white hover:bg-cecOrange font-medium rounded-lg text-sm md:text-md lg:text-lg px-5 py-2.5 text-center me-2 mb-2 flex gap-1 justify-center items-center"
         >
           <DoorOpen size={30} />
+          <p>Check-in</p>
+        </button>
+        <button
+          purpose="Check-Out"
+          onClick={() => {
+            setPurpose("Check-out");
+            setHideModal(!hideModal);
+          }}
+          type="button"
+          className="text-cecRed w-[250px] hover:text-white border border-cecRed bg-white hover:bg-cecRed font-medium rounded-lg text-sm md:text-md lg:text-lg px-5 py-2.5 text-center me-2 mb-2 flex gap-1 justify-center items-center"
+        >
+          <DoorClosed size={30} />
           <p>Check-out</p>
         </button>
+      </div>
+      {/* Modal for CheckIn/CheckOut */}
+
+      <div
+        id="default-modal"
+        tabIndex="-1"
+        aria-hidden="true"
+        className={`${
+          hideModal && "hidden"
+        } fixed inset-0 z-[70] flex justify-center items-center backdrop-blur-sm`}
+      >
+        <div className="z-100 relative p-4 w-full max-w-2xl max-h-full">
+          <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
+            <div className="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
+              <h3 className="text-xl font-semibold text-gray-900 dark:text-white">
+                Are you sure you want to {purpose} this visitor?
+              </h3>
+              <button
+                type="button"
+                className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
+                /* data-modal-hide="default-modal" */
+                onClick={() => {
+                  setHideModal(!hideModal);
+                }}
+              >
+                <span className="sr-only">Close modal</span>
+              </button>
+            </div>
+
+            <div className="flex items-center p-4 md:p-5 border-t border-gray-200 rounded-b dark:border-gray-600">
+              <button
+                type="button"
+                onClick={checkVisitorInOrOut}
+                className="text-white bg-cecOrange hover:bg-[#8A5F00] focus:ring-4 focus:outline-none focus:ring-[#8A5F00] font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+              >
+                Yes
+              </button>
+              <button
+                onClick={() => {
+                  setHideModal(!hideModal);
+                }}
+                type="button"
+                className="py-2.5 px-5 ms-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-cecOrange focus:z-10 focus:ring-4 focus:ring-cecOrange  "
+              >
+                No
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
